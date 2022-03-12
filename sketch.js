2146de32ebe61161476bc5a8ac75206fbf798f01
Thynguyen252 <hoangbaothy.nguyen@sjsu.edu>
2022-03-12 08:13:28 -0800

    Update sketch.js
    making random

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 // capitals in the world
 
-let capitals = ["Berlin", "Paris", "Tokyo", "London", "Rome", "Keiv", "Vienna"];
-
+let capitals = [
+  {name: "Berlin", color: "red"},
+  {name: "Paris", color: "blue"},
+  {name: "Tokyo", color: "pink"},
+  {name: "London", color: "green"},
+  {name: "Rome", color: "brown"},
+  {name: "Keiv", color: "purple"},
+  {name: "Vienna", color: "gray"},
+];
+ let randomIndex;
 function setup() {
   createCanvas(600, 600);
   background(200);
@@ -9,7 +17,7 @@ function setup() {
   // capitals[0] = "Berlin germany";
   // console.log(capitals);
 
-  console.log("initial array is");
+  /*console.log("initial array is");
   console.log(capitals);
 
   capitals.pop();
@@ -18,7 +26,28 @@ function setup() {
 
   capitals.push("Hanoi");
   console.log("array after push");
-  console.log(capitals);
+  console.log(capitals);*/
+
+  // conslole.log(capitals[0].name);
+
+  // console.log(capitals[int(random(3))]);
+
+/*randomIndex = int(random(capitals.length));
+  console.log(capitals[randomIndex].name);
+  text(capitals[randomIndex].name, 200, 200);
+
+capitals.splice(randomIndex,1);
+console.log(capitals);*/
+
+
 }
 
-function draw() {}
+function draw() {
+}
+
+function mousePressed(){
+  background(random(200, 255));
+  randomIndex = int(random(capitals.length));
+  text(capitals[randomIndex].name, 200, 200);
+  capitals.splice(randomIndex,1);
+}
